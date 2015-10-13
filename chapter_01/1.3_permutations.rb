36d3153ef572c136e9a649adff895c35d781c663
John Cipriano <johnmikecip@gmail.com>
2015-10-12 22:38:53 -0700

    fixed 1.3 tests & solved 1.3

--- chapter_01/1.3_permutations.rb
+++ chapter_01/1.3_permutations.rb
@@ -2,6 +2,19 @@
 #   "Given two strings, decide if one is a permutation of the other."
 #   Cracking the Coding Interview, p 73
 
-def is_permutation?(a, b)
-  true
+def is_permutation_hash?(a, b)
+  # O(n) for characters
+  freq = Hash.new(0)
+
+  a.chars.each { |c| freq[c] += 1 }
+  b.chars.each { |c| freq[c] -= 1 }
+
+  freq.values.all? { |val| val == 0 }
 end
+
+def is_permutation_sort?(a, b)
+  # O(n log n) but so clean
+  a.chars.sort == b.chars.sort
+end
+
+alias :is_permutation? :is_permutation_hash?
--- spec/1.3_permutations_spec.rb
+++ spec/1.3_permutations_spec.rb
@@ -1,17 +1,22 @@
 require_relative '../chapter_01/1.3_permutations'
 
+# Note: tests updated as creator did not understand permutations
+
 describe :is_permutation? do
   it 'returns true if B is empty' do
-    expect(is_permutation?('123', '')).to be true
     expect(is_permutation?('', '')).to be true
   end
 
-  it 'returns false if B is longer than A' do
-    expect(is_permutation?('123', '12345')).to be false
-    expect(is_permutation?('123', '12323')).to be false
+  it 'returns true if A and B are equal' do
+    expect(is_permutation?('abc', 'abc')).to be true
   end
 
-  it 'returns true if B is a single element of A' do
-    expect(is_permutation?('123', '1')).to be true
+  it 'returns false if B is longer than A' do
+    expect(is_permutation?('abc', 'abcde')).to be false
+    expect(is_permutation?('abc', 'abcbc')).to be false
+  end
+
+  it 'returns true for a simple case' do
+    expect(is_permutation?('abc', 'bac')).to be true
   end
 end
