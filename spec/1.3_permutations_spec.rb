55d46bc4b3c89c8a6eea0e3b752ce05aadeadf80
John Cipriano <johnmikecip@gmail.com>
2015-10-12 22:41:34 -0700

    refacted 1.3 test

--- spec/1.3_permutations_spec.rb
+++ spec/1.3_permutations_spec.rb
@@ -3,20 +3,25 @@ require_relative '../chapter_01/1.3_permutations'
 # Note: tests updated as creator did not understand permutations
 
 describe :is_permutation? do
-  it 'returns true if B is empty' do
+  let (:input) { 'abc' }
+
+  it 'returns true if A and B are empty' do
     expect(is_permutation?('', '')).to be true
   end
 
   it 'returns true if A and B are equal' do
-    expect(is_permutation?('abc', 'abc')).to be true
+    expect(is_permutation?(input, input)).to be true
   end
 
-  it 'returns false if B is longer than A' do
-    expect(is_permutation?('abc', 'abcde')).to be false
-    expect(is_permutation?('abc', 'abcbc')).to be false
+  it 'returns false if B is longer' do
+    expect(is_permutation?(input, input + 'a')).to be false
+  end
+
+  it 'returns false if A is longer' do
+    expect(is_permutation?(input + 'a', input)).to be false
   end
 
   it 'returns true for a simple case' do
-    expect(is_permutation?('abc', 'bac')).to be true
+    expect(is_permutation?(input, 'bac')).to be true
   end
 end
